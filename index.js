41f06cc81111452251963e84ef6d9cff9182c4f4
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-04-10 06:09:55 +0600

    adding_sounds

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var buttonColors=["red","green","blue","yellow"];
 var buttonPattern=[];
-function nexSequence(){
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+
+});
+function nextSequence(){
     var buttonGenerate=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColors[buttonGenerate];
     buttonPattern.push(randomChosenColour);
+    playSound(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(10).fadeOut(100).fadeIn(100);
+    
+
+}
+function playSound(name){
+    var buttonSound= new Audio("sounds/"+name+".mp3");
+    buttonSound.play();
 }
