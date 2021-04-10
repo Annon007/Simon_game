77743b17ae684ea898729002ee1d6ae40ef16c69
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-04-10 06:17:04 +0600

    button_ animation

--- index.js
+++ index.js
@@ -8,18 +8,25 @@ $(".btn").click(function() {
 
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 
 });
 function nextSequence(){
     var buttonGenerate=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColors[buttonGenerate];
     buttonPattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-    $("#"+randomChosenColour).fadeIn(10).fadeOut(100).fadeIn(100);
-    
 
 }
 function playSound(name){
     var buttonSound= new Audio("sounds/"+name+".mp3");
     buttonSound.play();
+}
+function animatePress(colour){
+    $("#"+colour).addClass("pressed");
+     setTimeout(function(){
+        $("#"+colour).removeClass("pressed");
+
+     },100)
 }
