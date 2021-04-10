29d7bf6a232953686a5700cb844029b96ce2a473
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-04-10 06:20:47 +0600

    level_add

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-var buttonColors=["red","green","blue","yellow"];
-var buttonPattern=[];
+var buttonColors = ["red", "green", "blue", "yellow"];
+var buttonPattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-$(".btn").click(function() {
+$(document).keypress(function () {
+    if (!started) {
 
-  var userChosenColour = $(this).attr("id");
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
+$(".btn").click(function () {
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 
 });
-function nextSequence(){
-    var buttonGenerate=Math.floor(Math.random()*4);
-    var randomChosenColour=buttonColors[buttonGenerate];
+function nextSequence() {
+    level++;
+
+    $("#level-title").text("Level " + level);
+    var buttonGenerate = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColors[buttonGenerate];
     buttonPattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 
 }
-function playSound(name){
-    var buttonSound= new Audio("sounds/"+name+".mp3");
+function playSound(name) {
+    var buttonSound = new Audio("sounds/" + name + ".mp3");
     buttonSound.play();
 }
-function animatePress(colour){
-    $("#"+colour).addClass("pressed");
-     setTimeout(function(){
-        $("#"+colour).removeClass("pressed");
+function animatePress(colour) {
+    $("#" + colour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + colour).removeClass("pressed");
 
-     },100)
+    }, 100)
 }
