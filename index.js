4baa77c91237c5758cf5ad2fbf43e0663f95fb0e
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-04-10 06:33:09 +0600

    completed

--- index.js
+++ index.js
@@ -20,11 +20,48 @@ $(".btn").click(function () {
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+
 
 });
-function nextSequence() {
-    level++;
+function checkAnswer(currentLevel) {
 
+    if (buttonPattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === buttonPattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+      playSound("wrong");
+
+      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+
+
+    }
+
+}
+
+function nextSequence() {
+    
+    userClickedPattern = [];
+    level++;
     $("#level-title").text("Level " + level);
     var buttonGenerate = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[buttonGenerate];
@@ -44,3 +81,11 @@ function animatePress(colour) {
 
     }, 100)
 }
+function startOver() {
+
+    //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
+  
